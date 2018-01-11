c1d0efefff82c9b1c8bdd227130fe6fb22544cbd
cbm360 <camronbmoore@gmail.com>
2018-01-11 14:56:26 -0700

    Updating experince, hide portfolio and about sections while updating.

--- data/resumeData.js
+++ data/resumeData.js
@@ -1,148 +1,223 @@
 {
-    "name": "AboutMe",
-    "children": [
-    {
-        "name": "Qualifications",
-        "children": [
+   "name":"AboutMe",
+   "children":[
+      {
+         "name":"Qualifications",
+         "children":[
             {
-                "name": "Scrum",
-                "children": [
-                    {"name": "Project Backlog Grooming"},
-            				{"name": "Sprint Planning"},
-            				{"name": "Daily Stand-ups"},
-                    {"name": "Retrospectives"},
-					          {"name": "Sprint Demos"}
-                ]
+               "name":"Scrum",
+               "children":[
+                  {
+                     "name":"Product backlog grooming"
+                  },
+                  {
+                     "name":"Product roadmapping"
+                  },
+                  {
+                     "name":"Sprint planning"
+                  },
+                  {
+                     "name":"Daily stand-ups"
+                  },
+                  {
+                     "name":"Retrospectives"
+                  },
+                  {
+                     "name":"Sprint review/demo"
+                  }
+               ]
             },
             {
-                "name": "Presentations",
-                "children": [
-                    {"name": "Sprint Demo"},
-          					{"name": "Product Roll-out"},
-          					{"name": "Keynote Presentations"},
-          					{"name": "Employee Orientation"}
-                ]
+               "name":"Presentations",
+               "children":[
+                  {
+                     "name":"Product roll-out"
+                  },
+                  {
+                     "name":"Keynote presentations"
+                  },
+                  {
+                     "name":"Employee orientation"
+                  }
+               ]
             },
             {
-                "name": "Analytical Decision Making",
-                "children": [
-                    {"name": "Asset allocation"},
-                    {"name": "Vehicle fleet expansion"},
-                    {"name": "Operational hub location assessment"},
-                    {"name": "Evaluate staffing levels"},
-                    {"name": "Software project initiation"}
-                ]
+               "name":"Analytical Decision Making",
+               "children":[
+                  {
+                     "name":"Asset allocation"
+                  },
+                  {
+                     "name":"Vehicle fleet expansion"
+                  },
+                  {
+                     "name":"Operational hub location assessment"
+                  },
+                  {
+                     "name":"Evaluate staffing levels"
+                  },
+                  {
+                     "name":"Software project initiation"
+                  }
+               ]
             },
             {
-                "name": "Strategic Planning",
-                "children": [
-                    {"name": "Vehicle fleet expansion"},
-                    {"name": "Operational hub location evaluation"},
-                    {"name": "Executive Team annual strategic planning"},
-                    {"name": "New Product Development"}
-                ]
+               "name":"Strategic Planning",
+               "children":[
+                  {
+                     "name":"Vehicle fleet expansion"
+                  },
+                  {
+                     "name":"Operational hub location evaluation"
+                  },
+                  {
+                     "name":"Executive Team annual strategic planning"
+                  },
+                  {
+                     "name":"New Product Development"
+                  }
+               ]
             },
             {
-                "name": "Business Intelligence",
-                "children": [
-        					{"name": "Actionable organizational BI"},
-        					{"name": "Sales team BI"},
-        					{"name": "Vehicle asset allocation team BI"},
-        					{"name": "Consolidated executive dashboard"},
-        					{"name": "Sprint velocity reporting"}
-                ]
+               "name":"Business Intelligence",
+               "children":[
+                  {
+                     "name":"Actionable organizational BI"
+                  },
+                  {
+                     "name":"Sales team BI"
+                  },
+                  {
+                     "name":"Vehicle asset allocation team BI"
+                  },
+                  {
+                     "name":"Consolidated executive dashboard"
+                  },
+                  {
+                     "name":"Scrum team dashboards"
+                  }
+               ]
             },
             {
-                "name": "Team Building/Leadership",
-                "children": [
-                    {"name": "Training design and implementation"},
-                    {"name": "Mentorship"},
-                    {"name": "Team Dynamics"},
-                    {"name": "Team Productivity"},
-					          {"name": "Servant Leadership"}
-                ]
-            },
-			{
-                "name": "Vendor Management",
-                "children": [
-                    {"name": "Scope of Work Definition"},
-                    {"name": "Service Level Agreement Oversight"},
-                    {"name": "Price Negotiation"}
-                ]
+               "name":"Team Building/Leadership",
+               "children":[
+                  {
+                     "name":"Training design and implementation"
+                  },
+                  {
+                     "name":"Mentorship"
+                  },
+                  {
+                     "name":"Team Dynamics"
+                  },
+                  {
+                     "name":"Team Productivity"
+                  },
+                  {
+                     "name":"Servant Leadership"
+                  }
+               ]
             }
-        ]
-    },
-    {
-        "name": "Technical Skills",
-        "children": [
+         ]
+      },
+      {
+         "name":"Technical Skills",
+         "children":[
             {
-                "name": "JavaScript",
-                "children": [
-                    {"name": "jQuery"},
-                    {"name": "Mocha"},
-                    {"name": "React"},
-                    {"name": "Redux"},
-                    {"name": "Electron"},
-                    {"name": "Knockout"},
-                    {"name": "MapBox/Leaflet"},
-                    {"name": "MapQuest"},
-                    {"name": "Node"},
-                    {"name": "Express"}
-                ]
-            },
-            {
-                "name": "Web",
-                "children": [
-                    {"name": "HTML5"},
-                    {"name": "CSS3"},
-                    {"name": "SASS"},
-                    {"name": "Bootstrap"},
-                    {"name": "Chrome Dev Tools"}
-                ]
-            },
-            {"name": "GIT"},
-			{
-                "name": "Build Tools",
-                "children": [
-                    {"name": "Gulp"},
-                    {"name": "Webpack"}
-                ]
+               "name":"JavaScript",
+               "children":[
+                  {
+                     "name":"ES6"
+                  },
+                  {
+                     "name":"Mocha"
+                  },
+                  {
+                     "name":"React"
+                  },
+                  {
+                     "name":"Redux"
+                  },
+                  {
+                     "name":"Node"
+                  },
+                  {
+                     "name":"Express"
+                  }
+               ]
             },
             {
-                "name": "ALM Software",
-                "children": [
-                    {"name": "Visual Studio Online"},
-                    {"name": "CA Agile"}
-                ]
-            },
-			{
-                "name": "Microsoft Suite",
-                "children": [
-                    {"name": "Word"},
-          					{"name": "Excel"},
-          					{"name": "PowerPoint"},
-                    {"name": "PowerPivot"},
-          					{"name": "Visio"},
-          					{"name": "Project"},
-          					{"name": "Visual Studio"}
-                ]
-            },
-            {
-                "name": "Database",
-                "children": [
-                    {"name": "SQL Server"},
-                    {"name": "MongoDB"},
-					          {"name": "SQLite"}
-                ]
+               "name":"Web",
+               "children":[
+                  {
+                     "name":"HTML5"
+                  },
+                  {
+                     "name":"CSS3"
+                  },
+                  {
+                     "name":"SASS"
+                  },
+                  {
+                     "name":"Bootstrap"
+                  },
+                  {
+                     "name":"Chrome Dev Tools"
+                  }
+               ]
             },
             {
-                "name": "Mobile",
-                "children": [
-                    {"name": "React-Native"}
-                ]
+               "name":"GIT"
+            },
+            {
+               "name":"Build Tools",
+               "children":[
+                  {
+                     "name":"Gulp"
+                  },
+                  {
+                     "name":"Webpack"
+                  }
+               ]
+            },
+            {
+               "name":"Microsoft Suite",
+               "children":[
+                  {
+                     "name":"Word"
+                  },
+                  {
+                     "name":"Excel"
+                  },
+                  {
+                     "name":"PowerPoint"
+                  },
+                  {
+                     "name":"PowerPivot"
+                  },
+                  {
+                     "name":"Visio"
+                  },
+                  {
+                     "name":"Project"
+                  }
+               ]
+            },
+            {
+               "name":"Database",
+               "children":[
+                  {
+                     "name":"SQL Server"
+                  },
+                  {
+                     "name":"MongoDB"
+                  },
+                  {
+                     "name":"SQLite"
+                  }
+               ]
             }
-        ]
-    }
-]
-
+         ]
+      }
+   ]
 }
