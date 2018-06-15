4c74110111de4b47017e5136ac864aa171a1be28
Camron Moore <camronbmoore@gmail.com>
2018-06-14 18:56:20 -0600

    Created react app skeleton.

---
+++ react/my-app/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './footer.css'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>Copyright 2018 &copy; www.CamronMoore.com</div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
